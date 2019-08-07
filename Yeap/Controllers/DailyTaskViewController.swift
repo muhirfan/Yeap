6e38bd34e4e9a89991434f25065aaf802a731c58
Irfan <muhirfan.mi@gmail.com>
2019-08-07 17:07:39 +0700

    add collection to daily task

--- Yeap/Controllers/DailyTaskViewController.swift
+++ Yeap/Controllers/DailyTaskViewController.swift
@@ -8,14 +8,41 @@
 
 import UIKit
 
-class DailyTaskViewController: UIViewController {
-
+class DailyTaskViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var taskCollectionView: UICollectionView!
+    
+    fileprivate var tasks = Task.create()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        taskCollectionView.dataSource = self
+        taskCollectionView.delegate = self
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Task Cell", for: indexPath as IndexPath) as! DailyTaskCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.taskLabel.text = tasks[indexPath.item].title
+    
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
 
     /*
     // MARK: - Navigation
---
+++ Yeap/DailyTaskCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DailyTaskCollectionViewCell.swift
+//  Yeap
+//
+//  Created by Muhammad Irfan on 07/08/19.
+//  Copyright © 2019 House of Do. All rights reserved.
+//
+
+import UIKit
+
+class DailyTaskCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var taskImageView: UIImageView!
+    @IBOutlet weak var taskLabel: UILabel!
+    
+}
