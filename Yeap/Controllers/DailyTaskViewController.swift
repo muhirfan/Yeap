c9ff2dbafbcf3776ea1a6ca6a07422ad0ddb304a
agatha <agatha.rachmat@gmail.com>
2019-08-07 13:41:07 +0700

    add Tabbar with 3 viewController

---
+++ Yeap/Controllers/DailyTaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DailyTaskViewController.swift
+//  Yeap
+//
+//  Created by Agatha Rachmat on 07/08/19.
+//  Copyright © 2019 House of Do. All rights reserved.
+//
+
+import UIKit
+
+class DailyTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
